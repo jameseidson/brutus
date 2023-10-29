5dc1834794205bc303f5be9f0e7f1e554b14d7a4
jameseidson <eidsonje@icloud.com>
2023-10-29 09:28:18 -0600

    basic io with pty

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use log::{debug, info};
+use nix::{pty, unistd};
+use std::{
+    env,
+    fs::File,
+    io,
+    os::fd::{FromRawFd, IntoRawFd},
+    process, thread,
+};
+use termion::{self, raw::IntoRawMode, screen::IntoAlternateScreen};
+
+fn main() {
+    simple_logger::init().unwrap();
+
+    info!("welcome to brutus");
+
+    let shell = env::var("SHELL").unwrap();
+    debug!("shell: {}", shell);
+    let (cols, rows) = termion::terminal_size().unwrap();
+
+    let forked = unsafe {
+        pty::forkpty(
+            Some(&pty::Winsize {
+                ws_row: rows,
+                ws_col: cols,
+                ws_xpixel: 0,
+                ws_ypixel: 0,
+            }),
+            Option::None,
+        )
+    }
+    .unwrap();
+
+    if let unistd::ForkResult::Child = forked.fork_result {
+        process::Command::new(&shell)
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+        std::process::exit(1);
+    }
+    let ptm_raw = forked.master.into_raw_fd();
+
+    thread::spawn(move || {
+        let mut ptm = unsafe { File::from_raw_fd(ptm_raw) };
+        io::copy(&mut io::stdin(), &mut ptm).unwrap();
+    });
+
+    thread::spawn(move || {
+        let mut ptm = unsafe { File::from_raw_fd(ptm_raw) };
+        let mut stdout = io::stdout()
+            .into_raw_mode()
+            .and_then(IntoAlternateScreen::into_alternate_screen)
+            .unwrap();
+        io::copy(&mut ptm, &mut stdout).unwrap();
+    });
+
+    loop {}
+}
